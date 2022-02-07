b66dc2b50471282f2ce1e49d4a7f6970528ea1d9
ztshat <ztshat@gmail.com>
2022-02-07 13:45:17 +0200

    add DB

--- deploy_2web-service.bat
+++
@@ -1,3 +0,0 @@
-xcopy /s /i /y "web_root\*" "\\\Secure FTP Connections\c-validator.com\opt\bitnami\apps\wordpress\htdocs\validator"
-start "" https://c-validator.com/validator/
-pause
---
+++ main_config.php
@@ -0,0 +1,37 @@
+<?php
+$b_is_local = true;
+//$s_domain = "https://c-validator.com/"; //remote server
+$s_domain = "http://192.168.1.200/";  //local server
+$b_use_access_wall = true;   //login required
+$b_do_debug = false;		 //do debug global
+
+$s_web_root = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR;
+$s_v_app_root = $s_web_root."validator".DIRECTORY_SEPARATOR;
+
+if($b_is_local == true){
+  //$s_domain = "https://c-validator.com/"; //remote server
+  $s_domain = "http://192.168.1.200/";   //local server
+  $_SESSION["s_domain"] = $s_domain;
+  $_SESSION["s_project_url"] = $s_domain . "/validator/";
+  $b_use_access_wall = false;
+  $_SESSION["s_user_name"] = "anonymous";
+  $_SESSION["s_user_email"] = "anonymous";
+}
+
+if($b_do_debug){
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  $_SESSION["b_debug"] = true;
+}else{
+  $_SESSION["b_debug"] = false;
+}
+function _get_task_url(){
+  return $_SESSION["s_domain"] . $_SESSION["s_task_page"];
+}
+function _dbg($data){
+  $output = $data;
+  if ( is_array( $output ) )
+    $output = implode( ',', $output);
+  echo "<script>console.log('" . addslashes($output) . "');</script>";
+}
+?>
